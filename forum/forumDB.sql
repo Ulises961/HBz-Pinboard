fae701c998ac22eff9b8632fff9803844e4171c1
Balawal Sultan <balawalsultan786@gmail.com>
2021-04-05 14:59:18 +0200

    created database to test forum

--- forum/forumDB.sql
+++ forum/forumDB.sql
@@ -1,35 +1,3 @@
-\c postgres;
-DROP database hbz;
-CREATE database hbz;
-\c hbz;
-
-CREATE TABLE Faculty(
-    code SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
-    
-); 
--- inclusion in program
-
-CREATE TABLE Program(
-    code SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    duration NUMERIC NOT NULL,
-    faculty INTEGER,
-
-    CONSTRAINT faculty_IsFKinProgram FOREIGN KEY(faculty)
-        REFERENCES faculty(code)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-);
-
-CREATE TABLE Subject(
-    id SERIAL PRIMARY KEY,
-    ECTs NUMERIC CHECK ( ECTs >= 0 AND ECTs <= 40 OR ECTs = -1),
-    name VARCHAR(300) UNIQUE
-);
---inclusion in Taught-in
-
 CREATE TABLE Users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
@@ -44,64 +12,6 @@ CREATE TABLE Users(
     CONSTRAINT valid_number CHECK (number ~ '^\d{4}\s\d{6}')
 );
 
-CREATE TABLE Professor(
-    id INTEGER PRIMARY KEY,
-    office_hours VARCHAR(200),
-    office VARCHAR(8),
-
-    CONSTRAINT professor_is_a_user FOREIGN KEY(id)
-        REFERENCES Users(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-);
-
-CREATE TABLE Taught_in(
-    subject INTEGER,
-    program INTEGER,
-    
-    CONSTRAINT PK_Taught_In PRIMARY KEY(subject, program),
-    CONSTRAINT subject_IsPKInTaughtIn FOREIGN KEY(subject)
-        REFERENCES subject(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-);
-CREATE TABLE Teaches(
-    professor INTEGER,
-    subject INTEGER,
-
-    CONSTRAINT PK_Teaches PRIMARY KEY(professor,subject),
-    CONSTRAINT professor_is_fk_in_Teaches FOREIGN KEY(professor)
-        REFERENCES Professor(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-        DEFERRABLE INITIALLY DEFERRED,
-
-        CONSTRAINT subject_is_fk_in_Teaches FOREIGN KEY(subject)
-        REFERENCES subject(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-
-);
-
-CREATE TABLE Student(
-    id INTEGER PRIMARY KEY,
-    program INTEGER NOT NULL,
-    
-    CONSTRAINT student_is_a_user FOREIGN KEY(id)
-        REFERENCES Users(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        DEFERRABLE INITIALLY DEFERRED,
-
-    CONSTRAINT programIsInStudent FOREIGN KEY(program)
-        REFERENCES Program(code)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-);
 
 CREATE TYPE FILETYPE AS ENUM ('png', 'jpeg', 'jpg');
 
@@ -256,75 +166,7 @@ CREATE TABLE Session(
         DEFERRABLE INITIALLY DEFERRED
 );
 
-CREATE TABLE Conversation(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    last_change DATE,
-    last_message VARCHAR(255) 
-);
-
-CREATE TABLE PartecipatesInConversation(
-    conversation INTEGER NOT NULL,
-    users INTEGER NOT NULL,
-    
-    PRIMARY KEY(conversation, users)
-);
-
-CREATE TABLE SendsMessageTo(
-    id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    text VARCHAR(255) NOT NULL,
-    users INTEGER NOT NULL,
-    conversation INTEGER NOT NULL,
-
-    CONSTRAINT message_belongs_to_user FOREIGN KEY(users)
-        REFERENCES Users(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        DEFERRABLE INITIALLY DEFERRED,
-    
-    CONSTRAINT message_is_sent_in_conversation FOREIGN KEY(conversation)
-        REFERENCES Conversation(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-);
-
-
 -- STORED PROCEDURES
-
-CREATE OR REPLACE FUNCTION insertSendsMessageTo()
-RETURNS TRIGGER AS $$
-BEGIN
-
-    IF NEW.time > LOCALTIME OR NEW.date > CURRENT_DATE OR NEW.date < CURRENT_DATE THEN 
-        RAISE EXCEPTION 'INVALID TIME OR DATE!';
-        RETURN NULL;
-    ELSE
-        RETURN NEW;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION isUserPartOfConversation()
-RETURNS TRIGGER AS $$
-DECLARE participation RECORD;
-BEGIN
-
-    SELECT EXISTS(  SELECT * 
-                    FROM PartecipatesInConversation 
-                    WHERE users = NEW.users AND conversation = NEW.conversation
-                ) INTO participation;
-
-    IF participation.exists THEN 
-        RETURN NEW;     
-    ELSE
-        RAISE EXCEPTION 'THE USER IS NOT A PARTECIPANT OF THIS CONVERSATION';
-        RETURN NULL;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION is_post_a_question()
 RETURNS TRIGGER AS $$
@@ -368,74 +210,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION update_conversation()
-RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE conversation
-    SET last_change = NEW.date, last_message = NEW.text
-    WHERE id = NEW.conversation;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION is_user_logged_in()
-RETURNS TRIGGER AS $$
-DECLARE active_session RECORD;
-BEGIN
-    SELECT EXISTS(  SELECT * 
-                    FROM Session 
-                    WHERE id = NEW.users AND end_time IS NOT NULL AND date = CURRENT_DATE
-                ) INTO active_session;
-
-    IF active_session.exists THEN 
-        RETURN NEW;
-    ELSE
-        RAISE EXCEPTION 'You must be logged in to do the requested operation';
-        RETURN NULL;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION is_user_professor()
-RETURNS TRIGGER AS $$
-DECLARE professor RECORD;
-BEGIN
-    SELECT EXISTS( SELECT * FROM Professor WHERE id = NEW.id) INTO professor;
-
-    IF professor.exists THEN 
-        RETURN NULL;
-    ELSE
-        RETURN NEW;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION is_user_student()
-RETURNS TRIGGER AS $$
-DECLARE student RECORD;
-BEGIN
-    SELECT EXISTS( SELECT * FROM Student WHERE id  = NEW.id) INTO student;
-  
-    IF student.exists THEN
-        RETURN NULL;
-    ELSE
-        RETURN NEW;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
 
 -- TRIGGERS
-
-CREATE TRIGGER insert_sendsMessageTo
-BEFORE INSERT ON SendsMessageTo
-FOR EACH ROW EXECUTE
-PROCEDURE insertSendsMessageTo();
-
-CREATE TRIGGER is_user_part_of_conversation
-BEFORE INSERT ON SendsMessageTo
-FOR EACH ROW EXECUTE
-PROCEDURE isUserPartOfConversation();
 
 CREATE TRIGGER check_if_article_is_a_question
 BEFORE INSERT ON Article
@@ -467,41 +243,9 @@ BEFORE INSERT ON Answer
 FOR EACH ROW EXECUTE
 PROCEDURE is_post_an_article();
 
-CREATE TRIGGER update_conversation
-AFTER INSERT ON SendsMessageTo
-FOR EACH ROW EXECUTE
-PROCEDURE update_conversation();
+-- INSERTS
 
-CREATE TRIGGER insert_professor
-BEFORE INSERT ON Professor
-FOR EACH ROW EXECUTE
-PROCEDURE is_user_student();
-
-CREATE TRIGGER insert_student
-BEFORE INSERT ON Student
-FOR EACH ROW EXECUTE
-PROCEDURE is_user_professor();
-
--- THIS HAS BEEN COMMENTED FOR EASE OF DEVELOPMENT
--- CREATE TRIGGER  check_login_Post
--- BEFORE INSERT OR UPDATE OR DELETE ON Post
--- FOR EACH ROW EXECUTE PROCEDURE is_user_logged_in();
-
-CREATE TRIGGER check_login_SendsMessageTo
-BEFORE INSERT OR UPDATE OR DELETE ON SendsMessageTo
-FOR EACH ROW EXECUTE PROCEDURE is_user_logged_in();
-
-CREATE TRIGGER  check_login_PartecipatesInConversation
-BEFORE INSERT OR UPDATE OR DELETE ON PartecipatesInConversation
-FOR EACH ROW EXECUTE PROCEDURE is_user_logged_in();
-
-CREATE TRIGGER check_login_Vote
-BEFORE INSERT OR UPDATE OR DELETE ON Vote
-FOR EACH ROW EXECUTE PROCEDURE is_user_logged_in();
-
--- THIS HAS BEEN COMMENTED FOR EASE OF DEVELOPMENT
--- CREATE TRIGGER check_login_Comment
--- BEFORE INSERT OR UPDATE OR DELETE ON Comment
--- FOR EACH ROW EXECUTE PROCEDURE is_user_logged_in();
+INSERT INTO Users VALUES(default, 'bob', 'freeman', '+39', '1234 567890', 'bob@gmail.com', 'password');
+INSERT INTO Users VALUES(default, 'Frank', 'Miller', '+39', '1234 567890', 'frank@gmail.com', 'password');
 
 
