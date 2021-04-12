10e8da1aa94578407d9b35293eb5c4446d3d09ae
Balawal Sultan <balawalsultan786@gmail.com>
2021-04-12 10:10:58 +0200

    resolved missing files issue

--- register.php
+++ register.php
@@ -15,11 +15,7 @@
     <!--===============================================================================================-->
     <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
 
-    <!-- Icons font CSS-->
-    <link href="vendor/mdi-font/css2/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css1/font-awesome.min.css" rel="stylesheet" media="all">
     <!-- Font special for pages-->
-   
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
         rel="stylesheet">
 
