8b6aeabe68d8c9799dac0682829b0817c3c5300e
Ulises E. Sosa <ulyses.sosa@gmail.com>
2021-04-19 11:12:14 +0200

    changed css

--- Landing.php
+++ Landing.php
@@ -12,14 +12,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="css/team.css">
+    <link rel="stylesheet" href="css/home_main.css">
 
     
 
 </head>
-
-
-
 
     <body>
 
