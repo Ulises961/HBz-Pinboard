1cb7bdf20c967005fc41800eff09e557f94ae37f
Balawal Sultan <balawalsultan786@gmail.com>
2021-06-05 14:34:25 +0200

    added code for session check on db

---
+++ php/session_php/closeSession.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+function closeSession($user, $dbh){
+    
+    $date = date("d/m/y");
+    $time = date("H:i:s");
+
+    $closeSession = "UPDATE Session ".
+                    "SET end_time = :endTime ". 
+                    "WHERE end_time = NULL AND users = :user";
+
+    $query = $dbh->prepare($closeSession);
+    
+    $query->bindParam(':endTime', $time, PDO::PARAM_STR);
+    $query->bindParam(':user', $user, PDO::PARAM_INT);
+
+    $query->execute();
+}
+    
+?>
--- php/session_php/logout.php
+++ php/session_php/logout.php
@@ -1,7 +1,15 @@
 <?php
 
+include "../registration_php/credentials.php";
+
+include "closeSession.php";
+
 session_start(['cookie_lifetime' => 43200,'cookie_secure' => true,'cookie_httponly' => true]);
 // remove all session variables
+$dbh = new PDO($conn_string);
+
+closeSession($_SESSION["user_id"], $dbh);
+
 session_unset();
 
 // destroy the session
