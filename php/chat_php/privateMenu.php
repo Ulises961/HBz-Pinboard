7412edcfb92d404e8cca72649c46170e1d1644cf
Balawal Sultan <balawalsultan786@gmail.com>
2021-06-10 11:10:25 +0200

    improved block feature

--- php/chat_php/privateMenu.php
+++ php/chat_php/privateMenu.php
@@ -6,19 +6,22 @@
         
         $dbh = new PDO($conn_string);
 
-        $getPrivateConversation = "SELECT wasBlockedBy FROM PrivateConversation WHERE id = :conversation";
+        $getPrivateConversation = "SELECT wasBlockedBy, blocked FROM PrivateConversation WHERE id = :conversation";
         $query = $dbh->prepare($getPrivateConversation);
 
         $query->bindParam(":conversation", $_REQUEST["conversation"], PDO::PARAM_INT);
         $query->execute();
         
         $privateConversation = $query->fetch();
+        $chatIsBlocked = $privateConversation["blocked"];
         $chatWasBlockedBy = $privateConversation["wasblockedby"];
 
-        if($chatWasBlockedBy != NULL && $chatWasBlockedBy != $_SESSION["user_id"])
+        if($chatIsBlocked && $chatWasBlockedBy != $_SESSION["user_id"])
             echo "<p>You have been blocked</p>";
+        elseif($chatIsBlocked && $chatWasBlockedBy == $_SESSION["user_id"])
+            echo "<button class='btn btn-danger' onclick='blockConversation()'>Unblock User</button>";
         else
-            echo "<button class='btn btn-info' onclick='blockConversation()'>Block User</button>";   
+            echo "<button class='btn btn-info' onclick='blockConversation()'>Block User</button>";
     }
 
 ?>
